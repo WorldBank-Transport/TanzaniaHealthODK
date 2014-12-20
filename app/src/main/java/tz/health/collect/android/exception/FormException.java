b50b4ca26a86b82b1dcd1fc5b75e375531abeb3c
machakux <bmachaku@gmail.com>
2014-12-20 08:54:23 +0300

    Fix package name typo

---
+++ app/src/main/java/tz/health/collect/android/exception/FormException.java
@@ -0,0 +1,14 @@
+package tz.health.collect.android.exception;
+
+public class FormException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public FormException (String msg) {
+		super(msg);
+	}
+
+}
